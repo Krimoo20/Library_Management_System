905a48bfa8581a43170dd37868e5e83985244d93
Krimoo20 <98287762+Krimoo20@users.noreply.github.com>
2022-12-27 21:17:37 +0100

    original

--- src/View/MainWindowController.java
+++ src/View/MainWindowController.java
@@ -10,12 +10,15 @@ import com.mysql.jdbc.PreparedStatement;
 import dataBaseHandeler.dataBaseConnection;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Timestamp;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -24,6 +27,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
@@ -57,6 +61,11 @@ public class MainWindowController implements Initializable {
     PreparedStatement pst = null;
     PreparedStatement pst2 = null;
     ResultSet rs=null;
+    boolean isReadyForSubmission=false;
+    @FXML
+    private TextField bookId;
+    @FXML
+    private ListView<String> issueBookList;
         /**
      * Initializes the controller class.
      */
@@ -185,5 +194,73 @@ public class MainWindowController implements Initializable {
         }
         }
         }
+
+    @FXML
+    private void loadIssuedBook(ActionEvent event) {
+        ObservableList<String>issueData=FXCollections.observableArrayList();
+        String id=bookId.getText();
+        String query1="select * from issuedbook where bookID='"+id+"'";
+        conn = dataBaseConnection.ConnectDb();
+        try {
+               pst = (PreparedStatement) conn.prepareStatement(query1);
+               ResultSet rs = pst.executeQuery();
+               while(rs.next()){
+                   String MemberID=rs.getString("memberID");
+                   Timestamp issueTime=rs.getTimestamp("issueTime");
+                   int renewCount= rs.getInt("renew_count");
+                   issueData.add("issue Date and Time:"+issueTime.toGMTString());
+                   issueData.add("Renew Count:"+renewCount);
+                   
+                   
+                   issueData.add("Book information:");
+                   query1="select * from book where id='"+id+"'";
+                   pst = (PreparedStatement) conn.prepareStatement(query1);
+                   ResultSet rs2 = pst.executeQuery();
+                   while(rs2.next()){
+                        issueData.add("Book Title:"+rs2.getString(2));
+                        issueData.add("Book Author:"+rs2.getString(3));
+                        issueData.add("Book Publisher:"+rs2.getString(4));
+                       
+                   }
+                   
+                   
+                   issueData.add("Member information:");
+                   query1="select * from members where id='"+MemberID+"'";
+                   pst = (PreparedStatement) conn.prepareStatement(query1);
+                   ResultSet rs3 = pst.executeQuery();
+                   while(rs3.next()){
+                        issueData.add("Member Name:"+rs3.getString(2));
+                        issueData.add("Member E-mail:"+rs3.getString(3));
+                        issueData.add("Member Mobile:"+rs3.getString(4));
+                   }
+                   isReadyForSubmission=true;
+            }
+               issueBookList.getItems().setAll(issueData);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+
+    @FXML
+    private void loadSubmission(ActionEvent event) {
+        if(!isReadyForSubmission){
+            return;
+        }
+        String id=bookId.getText();
+        String query1="delete from issuedbook where bookID='"+id+"'";
+        String query2="UPDATE `book` SET `Availble`='Available' WHERE id='"+id+"'";
+        conn = dataBaseConnection.ConnectDb();
+                try {
+            pst = (PreparedStatement)conn.prepareStatement(query1);
+            pst.execute();
+            pst2 =(PreparedStatement)conn.prepareStatement(query2);
+            pst2.execute();
+            JOptionPane.showMessageDialog(null, "The book has been submitted scessfully!!");
+            conn.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        
+    }
     }
     
